71a5fdc6b7789e05955f186ddb187c02a3221ada
Cedric Abuso <cedricabuso@gmail.com>
2015-10-29 12:57:13 +0800

    Uncommented sample

--- example/example.go
+++ example/example.go
@@ -19,7 +19,7 @@ func main() {
 		Access: "access.log",
 	})
 
-	/*log.Error("Error", logger.Data{
+	log.Error("Error", logger.Data{
 		"data": "Data details here",
 	})
 
@@ -36,7 +36,7 @@ func main() {
 	log.Info("Info", logger.Data{
 		"data": "Data details here",
 		"code": 123,
-	})*/
+	})
 
 	http.ListenAndServe("10.10.13.25:3000", SampleLogMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "helllo")
